80e5c9e7107956bc0a76614d9f3a7ab6ca81b50b
Yoshiki <rrrtcdish2@gmail.com>
2018-07-25 09:22:39 +0900

    Add Decorator

---
+++ decorator.py
@@ -0,0 +1,29 @@
+```
+	デコレータを使用する理由
+		あらゆる処理には必ず本来の機能とは関係ない前後の処理が存在している
+		その前後処理をデコレータで実現する
+```
+def calc(func):
+	def display(*args):
+		print('start')
+		print(func(*args))
+		print('end')
+	return display
+
+def plus(x, y):
+	return x + y
+
+print('関数呼び出し開始')
+calc(plus) (1, 2)
+print('関数呼び出し終了')
+
+print()
+
+# @シンボルでの利用
+@calc
+def plus2(x, y):
+	return x + y
+
+print('@での関数呼び出し開始')
+plus2(1, 2)
+print('@での関数呼び出し終了')
